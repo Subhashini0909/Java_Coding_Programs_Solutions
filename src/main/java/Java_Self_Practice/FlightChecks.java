8e09fca8fab2e8ff70f4348caa957593b81361c3
Subhashini0909 <c.subhashini09@gmail.com>
2025-07-08 15:39:44 +0530

    Flight check

---
+++ src/main/java/Java_Self_Practice/FlightChecks.java
@@ -0,0 +1,33 @@
+package Java_Self_Practice;
+
+import java.util.Scanner;
+
+public class FlightChecks {
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		System.out.println("Welcome to Security Check in");
+		
+		Thread.sleep(3000);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Are you coming from connecting flight(Yes/No)? ");
+		String Yes_No = sc.next();
+		Thread.sleep(3000);
+		
+//		if(Yes_No.equalsIgnoreCase("yes"))
+//		{
+//			System.out.println("Enjoy your fight");
+//		}
+//		else if(Yes_No.equalsIgnoreCase("No"))
+//		{
+//			System.out.println("Please proceed to security checks");
+//		}
+		
+		if(Yes_No.equalsIgnoreCase("no"))
+		{
+			System.out.println("Please proceed to security checks");
+		}
+		System.out.println("Enjoy your fight");
+	}
+
+}
